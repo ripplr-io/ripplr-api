e5a62c3c71e8c15c91a4abee119c8817dc619f7c
Guilherme Graça <ggracase@gmail.com>
2020-07-21 20:29:24 +0000

    User Authentication

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,16 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
   # TODO: Add authentication to sidekiq routes
   mount Sidekiq::Web, at: '/sidekiq'
+
+  # API
+  defaults format: :json do
+    devise_for :users, path: 'auth', path_names: {
+      sign_in: :login,
+      sign_out: :logout,
+      registration: :signup
+    }, controllers: {
+      sessions: :sessions,
+      registrations: :registrations
+    }
+  end
 end
