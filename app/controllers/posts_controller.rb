6b03cf5570698954c94edb2f1f76a5e5d68c8ae2
Guilherme Graca <ggracase@gmail.com>
2020-09-11 18:35:08 +0100

    Add pagination to posts#index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   wrap_parameters :post
 
   def index
-    data = ActiveModelSerializers::SerializableResource.new(find_posts).as_json
+    data = ActiveModelSerializers::SerializableResource.new(find_paginated_posts).as_json
     render json: { data: data }
   end
 
@@ -25,6 +25,10 @@ class PostsController < ApplicationController
     return User.friendly.find(params[:user_id]).posts if params[:user_id].present?
     return Topic.friendly.find(params[:topic_id]).posts if params[:topic_id].present?
     return Hashtag.find_by(name: params[:hashtag_id]).posts if params[:hashtag_id].present?
+  end
+
+  def find_paginated_posts
+    find_posts.page(params[:page]).per(params[:per_page])
   end
 
   def post_params
