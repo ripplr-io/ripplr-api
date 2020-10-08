cbe7cf1b2d548b6bea45f4253d9fb34c28530578
Guilherme Graça <GGracaSe@gmail.com>
2020-10-08 21:04:59 +0100

    Env specific credentials (#3)
    * Add env specific credentials
    
    * Add test master key to github

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -310,7 +310,7 @@ Devise.setup do |config|
   # config.sign_in_after_change_password = true
 
   config.jwt do |jwt|
-    jwt.secret = Rails.application.credentials.jwt_secret
+    jwt.secret = Rails.application.credentials.secret_key_base
 
     jwt.dispatch_requests = [
       ['POST', %r{^/auth/login$}]
