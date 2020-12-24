ac13ba8359a633c10e2d5278bf15e5ae3c92dfae
Guilherme Graca <ggracase@gmail.com>
2020-12-24 02:19:14 +0000

    Fix alert

--- app/workers/alerts/invoice_payment_worker.rb
+++ app/workers/alerts/invoice_payment_worker.rb
@@ -3,11 +3,12 @@ module Alerts
     PAYMENT_INTENTS_URL = 'https://dashboard.stripe.com/test/payments/'.freeze
 
     def perform(customer_id, paid, intent_id)
+      pp intent_id
       billing = Billing.find_by(stripe_customer_id: customer_id)
       user_name = billing&.user&.name || 'unknown user'
 
       message = build_message(user_name, paid, intent_id)
-
+      pp message
       Slack::NotifyService.new.ping(message, '#stripe')
     end
 
@@ -15,7 +16,7 @@ module Alerts
 
     def build_message(user_name, paid, intent_id)
       prefix = paid ? 'New payment' : 'Payment failed'
-      prefix + " from #{user_name}:\n#{PAYMENT_INTENTS_URL + intent_id}"
+      "[#{prefix}](#{PAYMENT_INTENTS_URL}#{intent_id}) (#{user_name})!"
     end
   end
 end
