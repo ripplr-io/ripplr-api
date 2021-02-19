106ee07ede6cf7a9c57c8c805cc72285117f877c
Guilherme Graça <GGracaSe@gmail.com>
2021-02-19 14:37:46 +0000

    Fix subscription count (#104)

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -3,7 +3,7 @@ class Subscription < ApplicationRecord
   belongs_to :subscribable, polymorphic: true
 
   has_many :subscription_inboxes, dependent: :destroy
-  has_many :inboxes, through: :subscription_inboxes # TODO: validate it has at least one
+  has_many :inboxes, through: :subscription_inboxes, dependent: :destroy
 
   validates :subscribable_id, uniqueness: { scope: [:subscribable_type, :user_id] }
 end
