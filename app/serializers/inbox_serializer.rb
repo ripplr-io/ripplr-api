55355f4695dc93b794489782e9f501adce40e5cd
Guilherme Graça <GGracaSe@gmail.com>
2021-02-19 14:59:59 +0000

    Add inbox_items_not_archived_counter (#105)

--- app/serializers/inbox_serializer.rb
+++ app/serializers/inbox_serializer.rb
@@ -1,5 +1,9 @@
 class InboxSerializer < ApplicationSerializer
   attributes :name, :settings, :description, :inbox_items_count, :inbox_items_archived_count, :subscription_inboxes_count
 
+  attribute :inbox_items_not_archived_count do |object|
+    object.inbox_items_count - object.inbox_items_archived_count
+  end
+
   has_many :inbox_channels
 end
