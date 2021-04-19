7124e2337d0f8f45fdbb8a7e9cd8e0d3b59bf6f2
Guilherme Graça <GGracaSe@gmail.com>
2021-04-19 15:39:25 +0100

    Remove points validation (#157)
    * Remove points validation
    
    * Fix tests

--- app/interactors/communities/create.rb
+++ app/interactors/communities/create.rb
@@ -1,23 +1,9 @@
 module Communities
   class Create < ApplicationInteractor
-    REQUIRED_POINTS = 1000
-
-    # TODO: move to validator
-    before :validate_user_points
-
     def call
       context.fail! unless context.resource.save
 
       Segment::TrackCommunityCreatedWorker.perform_async(context.resource.id)
-    end
-
-    private
-
-    def validate_user_points
-      return if context.resource.owner.total_points >= REQUIRED_POINTS
-
-      context.resource.errors.add(:total_points, 'below required amount')
-      context.fail!
     end
   end
 end
--- spec/requests/communities/create_spec.rb
+++ spec/requests/communities/create_spec.rb
@@ -5,27 +5,18 @@ RSpec.describe :communities_create, type: :request do
     let(:subject) { post communities_path }
   end
 
-  it_behaves_like :unprocessable_request, [:total_points] do
-    let(:subject) do
-      user = create(:prize, points: 100).user
-      post communities_path, headers: auth_headers_for(user)
-    end
-  end
-
   it_behaves_like :unprocessable_request, [:name, :description, :community_topics] do
     let(:subject) do
-      user = create(:prize, points: 1000).user
-      post communities_path, headers: auth_headers_for(user)
+      post communities_path, headers: auth_headers_for_new_user
     end
   end
 
   it 'responds with the community' do
-    user = create(:prize, points: 1000).user
     topic = create(:topic)
 
     post communities_path,
       params: attributes_for(:community).slice(:name, :description).merge(topic_ids: [topic.id].to_json),
-      headers: auth_headers_for(user)
+      headers: auth_headers_for_new_user
 
     expect(response).to have_http_status(:created)
     expect(response_data).to have_resource(Community.last)
